c5158f3ff9e06e16701b95548b61b05f4e5a33fe
BK <3031011@naver.com>
2023-04-10 18:03:51 +0900

    JavaScript 연습문제 풀이

--- JavaScript/JavaScript Study/연습문제.js
+++ JavaScript/JavaScript Study/연습문제.js
@@ -5,27 +5,45 @@ let sum = arr.reduce((acc, cur) => {
     return acc + cur;
 }, 0)
 const result = sum / arr.length;
-const result1 = (sum - result) ** 2;
+// 분산
+const result1 = arr.reduce((acc1, cur1) => {
+    return acc1 += ((cur1 - result) ** 2) / arr.length;
+}, 0)
+// 각 값 - 평균 ** 2 / 6
 
-console.log(result)
-console.log(result1)
 
 // 연습문제 2번
-let str = '5, 4, 10, 2, 5';
-let arr2 = str.split(',');
+let str2 = '5, 4, 10, 2, 5';
+let arr2 = str2.split(',');
 let sum2 = 0;
-for (const key of arr) {
+for (const key of arr2) {
     let num = parseInt(key);
-    sum += num;
+    sum2 += num;
 }
 
-// 연습문제 3번
-const arr3 = [11, 22, 33, 111, 2].join('').slice('');
-let result2 = 0;
 
+// 연습문제 3번
+const arr3 = [11, 22, 33, 111, 2];
+const num3 = arr3.join('').slice('');
+let result3 = 0;
 for (var key of str) {
     result += parseInt(key)
 }
+
+
+// 연습문제 4번 반복문만 사용하여 숫자단위 콤마
+let userInput = prompt("");
+const str = userInput.toString();
+const num = parseInt(userInput);
+let result4 = '';
+for(let i = -1; i > -str.length; i--){
+    if(i % 3 === 0){
+        let x = str.slice(i-3, i);
+        result4 = x + ',' + result4;
+    }
+}
+result4 += str.slice(str.length - str.length-3 , str.length)
+
 
 // 연습문제 5번
 const arr5 = [];
