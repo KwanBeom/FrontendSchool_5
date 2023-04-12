f39778b81ac6f8a1291a7c5eb2ecfa6d2b8f8866
beomkwan.choi <3031011@naver.com>
2023-04-12 21:08:12 +0900

    JavaScript 전개구문 4/12 내용추가

---
+++ JavaScript/2023_0406/006_전개구문,구조분해할당.js
@@ -0,0 +1,116 @@
+// 전개구문
+// 배열의 전개 구문
+const 과일들 = ['사과', '파인애플', '수박'];
+const 생선들 = ['조기', '갈치', '다금바리'];
+const 합치면 = [...과일들, ...생선들];
+const 합치면2 = [과일들, 생선들];
+
+console.log(합치면);
+console.log(합치면2);
+
+// 객체의 전개 구문
+const 위니브1 = { 개리: 1, 빙키: 2 };
+const 위니브2 = { 라이캣: 3 };
+const 위니브3 = { ...위니브1, ...위니브2 };
+
+console.log(위니브3);
+
+
+const weniv = {
+    개리: '재직중',
+    빙키: '재직중',
+    라이켓: '재직중',
+    뮤라: '재직중',
+    해골왕: '재직중'
+};
+
+const weniv2 = {
+    ...위니브임직원,
+    라이켓: '휴가',
+    해골왕: '퇴사'
+}
+
+const weniv3 = {
+    ...위니브임직원
+}
+
+weniv
+
+
+// 구조분해할당
+for (const [i, j] of [[1, 2], [3, 4], [5, 6]]) {
+    console.log(i, j);
+}
+
+const 위니브임직원 = {
+    개리: '재직중',
+    빙키: '재직중',
+    라이켓: '재직중',
+    뮤라: '재직중',
+    해골왕: '재직중'
+};
+Object.entries(위니브임직원)
+
+for (const [i, j] of Object.entries(위니브임직원)) {
+    console.log(i, j);
+}
+
+let [a, b] = [10, 20]
+let [one, two, three] = '010-1234-5678'.split('-')
+let [first, second, ...third] = '010-1234-5678-123-123'.split('-')
+
+//////////////////////////////
+
+const test = [
+    [1, 2, [10, 20]],
+    [3, 4, [30, 40]],
+    [5, 6, [50, 60]]
+]
+for (const [i, j] of test) {
+    console.log(i, j);
+}
+
+const test1 = [
+    [1, 2, [10, 20]],
+    [3, 4, [30, 40]],
+    [5, 6, [50, 60]]
+]
+for (const [i, j, k] of test1) {
+    console.log(i, j, k);
+}
+
+const test2 = [
+    [1, 2, [10, 20]],
+    [3, 4, [30, 40]],
+    [5, 6, [50, 60]]
+]
+// i, j, k, l, m, n
+for (const [i, j, [k, l]] of test1) {
+    console.log(i, j, k, l);
+}
+
+// 이것은 기억해주세요.
+const test3 = [
+    [1, 2, 10, 20],
+    [3, 4, 30, 40],
+    [5, 6, 50, 60]
+]
+for (const [i, j, ...k] of test1) {
+    console.log(i, j, k);
+}
+
+function hello(a, b, ...c) {
+    console.log(a, b, c)
+}
+
+hello(1, 2, 3, 4, 5, 6, 7)
+
+
+// 4/12 배열의 최소 최대값
+Math.max(...[10, 20, 30])
+Math.min(...[10, 20, 30])
+Math.max(...[10, 20, 30, [11, 12, 13, [29, 40, 50]]].flat(Infinity))
+
+// 자주 사용되는 코드
+const s = 'hello world';
+[...s] // s.split('')
